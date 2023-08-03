82add1c0230e3971258e327a86f025375c430b2b
LucasR29 <lrigon2904@gmail.com>
2023-08-03 02:28:26 -0300

    changes: removed migrations files and created a new one

--- app/prisma/migrations/20230803052744_init/migration.sql
+++ app/prisma/migrations/20230803052744_init/migration.sql
@@ -7,23 +7,44 @@ CREATE TABLE "Collection" (
     "floorSale" JSONB NOT NULL,
     "floorSaleChange" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT "Collection_pkey" PRIMARY KEY ("id")
 );
+
 -- CreateTable
 CREATE TABLE "ColectionsSet" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT "ColectionsSet_pkey" PRIMARY KEY ("id")
 );
+
 -- CreateTable
-CREATE TABLE "_ColectionsSetToCollection" ("A" TEXT NOT NULL, "B" TEXT NOT NULL);
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ColectionsSetToCollection" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "_ColectionsSetToCollection_AB_unique" ON "_ColectionsSetToCollection"("A", "B");
+
 -- CreateIndex
 CREATE INDEX "_ColectionsSetToCollection_B_index" ON "_ColectionsSetToCollection"("B");
+
 -- AddForeignKey
-ALTER TABLE "_ColectionsSetToCollection"
-ADD CONSTRAINT "_ColectionsSetToCollection_A_fkey" FOREIGN KEY ("A") REFERENCES "ColectionsSet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_ColectionsSetToCollection" ADD CONSTRAINT "_ColectionsSetToCollection_A_fkey" FOREIGN KEY ("A") REFERENCES "ColectionsSet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
 -- AddForeignKey
-ALTER TABLE "_ColectionsSetToCollection"
-ADD CONSTRAINT "_ColectionsSetToCollection_B_fkey" FOREIGN KEY ("B") REFERENCES "Collection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_ColectionsSetToCollection" ADD CONSTRAINT "_ColectionsSetToCollection_B_fkey" FOREIGN KEY ("B") REFERENCES "Collection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
